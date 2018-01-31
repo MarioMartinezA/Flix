ca443f7c3d46e226c7cb8d30f6bdf9e1aeea744b
Mario Martinez <mart1041@m506-118-26440.local>
2018-01-31 15:50:07 -0800

    Added asset files

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NowPlayingViewController.swift
 //  Flix
 //
 //  Created by Mario Martinez on 1/31/18.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NowPlayingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    
 
 }
-
